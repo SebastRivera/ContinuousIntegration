57f8f50b6b8974082f28f6cfc400db4ae47c8249
Andrea Velez <86313886+anvefran@users.noreply.github.com>
2023-07-19 20:15:03 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-      
+        Meal bolon = new Meal(1, "Bolon", 5.00f, false, true); 
+        Meal tigrillo = new Meal(2, "Tigrillo", 5.00f, false, true); //special
+        Meal churrasco = new Meal(3, "Churrasco", 8.00f, false, false); //unavailable
+        Meal parrillada = new Meal(4, "Parrillada Premium", 30.00f, true, true); //special
+        Meal chicken = new Meal(5, "Chicken Parm", 18.50f, true, true); //special
         System.out.println("Dining Experience Manager");
         
     }
