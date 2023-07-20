b335a8437463c1902fed98f519e9c04adc5d39d4
Andrea Velez <86313886+anvefran@users.noreply.github.com>
2023-07-20 00:46:39 -0500

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,12 +1,25 @@
+import java.util.ArrayList; 
 
 public class Menu {
-    private Meal[] meals;
+    private ArrayList<Meal> meals;
 
-    public Meal[] getMeals() {
+    public Menu(ArrayList<Meal> meals){
+      this.meals = meals;
+    }
+    
+    public ArrayList<Meal> getMeals() {
         return meals;
     }
 
-    public void setMeals(Meal[] meals) {
+    public void setMeals(ArrayList<Meal> meals) {
         this.meals = meals;
+    }
+
+    public void displayMenu(){
+      System.out.println("Name - Price");
+      for (Meal m : this.meals) {
+        System.out.printf("%d. %s - %.2f\n", m.getId(), m.getName(), m.getPrice());
+        
+      }
     }
 }
